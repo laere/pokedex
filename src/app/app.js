f45141ef63a5ad49e01d4473c06b3b78562ce4da
Zack Bostian <bostianz@gmail.com>
2016-05-29 14:47:48 -0400

    added main container to app component, add in scss

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import PokedexContainer from '../containers/PokedexContainer';
+import '../../scss/main.scss';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-
+        <PokedexContainer />
       </div>
     );
   }
